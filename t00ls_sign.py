abaac14122dbae0941bf2d8116222e4baf455596
Slurse <Slurse@gmail.com>
2021-01-31 04:25:05 +0800

    Update t00ls_sign.py

--- t00ls_sign.py
+++ t00ls_sign.py
@@ -35,6 +35,8 @@ logindata = {
 
 rlogin = requests.post('https://www.t00ls.net/login.json', data = logindata)
 rlogj = json.loads(rlogin.text)
+print(rlogj)
+
 if (rlogj["status"] != "success"):
   print("登入失败，请检查输入资料是否正确！")
 else :
